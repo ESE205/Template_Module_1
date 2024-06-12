a4e2f2a18d2c1588f907168ebabf07d8651eeda6
James Feher <jdfeher@wustl.edu>
2024-06-12 15:45:22 -0500

    update module for FA24

---
+++ looper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys
+DEBUG     = False	# Turn on or off debug to help debug program
+loopCount = 5		# defaults to 5
+
+if (len(sys.argv) > 1): loopCount = int(sys.argv[1])
+if (len(sys.argv) > 2): DEBUG     = True if (int(sys.argv[2])> 0) else False
+if (DEBUG):  print (f'DEBUG: Number of times through loop: {loopCount}')
+
+i = 0
+while (i < loopCount):
+   i = i + 1
+   print (f'Looping: {i}')
+ 
+loopCount = int(input('Enter new loop count:'))
+ 
+i = 0
+while (i < loopCount):
+   i = i + 1
+   print (f'Looping: {i}')
