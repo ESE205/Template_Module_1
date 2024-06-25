544b708fa1920e82729a4c1aeea09d29e069aa74
James Feher <jdfeher@wustl.edu>
2024-06-25 16:24:48 -0500

    Updated looper

--- looper.py
+++ looper.py
@@ -6,6 +6,7 @@ loopCount = 5		# defaults to 5
 if (len(sys.argv) > 1): loopCount = int(sys.argv[1])
 if (len(sys.argv) > 2): DEBUG     = True if (int(sys.argv[2])> 0) else False
 if (DEBUG):  print (f'DEBUG: Number of times through loop: {loopCount}')
+if (DEBUG):  print (f'DEBUG: Name of program: {sys.argv[0]}')
 
 i = 0
 while (i < loopCount):
